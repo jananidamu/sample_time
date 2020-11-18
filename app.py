78eda4c58f8051204962806765f301b2f2192ec5
jananidamu <53175866+jananidamu@users.noreply.github.com>
2020-11-18 16:36:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def time():
     return str(datetime.now())
 
 if __name__ == '__main__':
-    app.run(debug=False , host='0.0.0.0', port=os.environ.get("PORT", 5000), threaded=True)
+    app.run()
